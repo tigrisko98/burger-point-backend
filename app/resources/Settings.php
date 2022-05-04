b57892cebcb0ca3cb3d1047e3060078f51c86a9c
tigrisko98 <kostia.tigrisko@gmail.com>
2022-05-04 14:43:26 +0300

    refactor settings resource

--- app/resources/Settings.php
+++ app/resources/Settings.php
@@ -6,15 +6,11 @@ class Settings extends \common\models\Settings
 {
     public function afterFind()
     {
-        $this->about_us_images_links = unserialize($this->about_us_images_links);
-        $this->restaurant_images_links = unserialize($this->restaurant_images_links);
-
-        if ($this->about_us_images_links === false && $this->restaurant_images_links === false) {
-            unset($this->about_us_images_links, $this->restaurant_images_links);
-        } elseif ($this->about_us_images_links) {
-            unset($this->restaurant_images_links);
-        } else {
-            unset($this->about_us_images_links);
+        if ($this->about_us_images_links !== null) {
+            $this->about_us_images_links = unserialize($this->about_us_images_links);
+        }
+        if ($this->restaurant_images_links !== null) {
+            $this->restaurant_images_links = unserialize($this->restaurant_images_links);
         }
     }
 }
